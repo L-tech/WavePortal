5c3ce228e2eb35cf6e0c7852048d357e51883137
L-tech <legendabrahamonoja@gmail.com>
2021-10-10 22:11:31 +0100

    update: add config settings

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,17 @@
 require('@nomiclabs/hardhat-waffle');
+require('dotenv').config();
 
 module.exports = {
   solidity: '0.8.0',
   networks: {
     rinkeby: {
-      url: 'https://eth-rinkeby.alchemyapi.io/v2/RXLgp3czbgJ6UMg_jIHcDi-zAU9vQa8e',
-      accounts: ['58340640d2c5f6d61b8e4f5ec430aaa10703e221ab1965bd545696a01bc8d804'],
+      url: process.env.STAGING_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    mainnet: {
+      chainId: 1,
+      url: process.env.PROD_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
 };
